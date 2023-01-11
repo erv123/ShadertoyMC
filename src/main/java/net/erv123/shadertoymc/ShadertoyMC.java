ffee4a50e24950b41e8311a67e7f46669c5becd0
erv123 <ervins.roderts@gmail.com>
2023-01-11 17:48:58 +0000

    initial commit

---
+++ src/main/java/net/erv123/shadertoymc/ShadertoyMC.java
@@ -0,0 +1,15 @@
+package net.erv123.shadertoymc;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ShadertoyMC implements ModInitializer {
+	public static final String MOD_ID = "shadertoymc";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+	}
+}
