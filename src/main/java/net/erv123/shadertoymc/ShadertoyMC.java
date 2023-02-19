fc39cda43256112c829161ab440cc39d8e45504e
erv123 <55497525+erv123@users.noreply.github.com>
2023-02-19 17:49:38 +0000

    update version number

--- src/main/java/net/erv123/shadertoymc/ShadertoyMC.java
+++ src/main/java/net/erv123/shadertoymc/ShadertoyMC.java
@@ -15,7 +15,7 @@ Make Area in Java
  */
 public class ShadertoyMC implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger("Shaydertoy");
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     public static MinecraftServer SERVER;
 
     @Override
